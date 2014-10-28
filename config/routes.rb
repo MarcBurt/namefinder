81a4fd684093708e434a60718a39dbdbc4b10bb5
Marc Burt <marc.burt@gmail.com>
2014-10-28 16:36:09 +0000

    further setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root to: "home#index"
-
+  root to: "users#index"
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
